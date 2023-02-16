ca4f50d51e62cb9b4355a46c4c3282fc6bb7aa14
Finnvb <finnvb@gmail.com>
2023-02-16 16:49:00 +0100

    .

--- plantswap/src/pages/index.js
+++ plantswap/src/pages/index.js
@@ -1,9 +1,13 @@
 import NavBar from "components/NavBar";
 
+import classes from "../styles/homepage.module.css";
 function HomePage() {
   return (
     <>
       <NavBar />
+      <main>
+        <img src="/hero-image.jpg" className={classes.backgroundImg} />
+      </main>
     </>
   );
 }
