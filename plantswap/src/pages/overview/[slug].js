68f196c6a61e66fb239178c490f82d77264ec31f
Finnvb <finnvb@gmail.com>
2023-02-23 15:17:37 +0100

    detailpage made

--- plantswap/src/pages/overview/[slug].js
+++ plantswap/src/pages/overview/[slug].js
@@ -7,15 +7,94 @@ function PlantDetailPage({ stekje }) {
   return (
     <>
       <NavBar />
-      <p>{stekje.naam}</p>
-      <img
-        className={classes.plantImg}
-        src={stekje.fotos[0].url}
-        alt={stekje.naam}
-      />
+      <h1 className={classes.header}>{stekje.naam}</h1>
+      <main className={classes.detailpageContainer}>
+        <section className={classes.plantContent}>
+          <img
+            className={classes.plantImgDetail}
+            src={stekje.fotos[0].url}
+            alt={stekje.naam}
+          />
 
-      {/* <div dangerouslySetInnerHTML={{ __html: stekje.zonlicht.html }}></div> */}
+          <div>
+            <h2>Temperatuur</h2>
 
+            <div
+              className={classes.paragraph}
+              dangerouslySetInnerHTML={{ __html: stekje.temperatuur.html }}
+            ></div>
+          </div>
+          <div>
+            <h2>Zonlicht</h2>
+            <div
+              className={classes.paragraph}
+              dangerouslySetInnerHTML={{ __html: stekje.zonlicht.html }}
+            ></div>
+          </div>
+          <div>
+            <h2>Stekken</h2>
+            <div
+              className={classes.paragraph}
+              dangerouslySetInnerHTML={{ __html: stekje.stekken.html }}
+            ></div>
+          </div>
+        </section>
+        <section className={classes.plantContent}>
+          <div>
+            <h2>Beschrijving</h2>
+            <div
+              className={classes.paragraph}
+              dangerouslySetInnerHTML={{ __html: stekje.beschrijving.html }}
+            ></div>
+          </div>
+          <div>
+            <h2>Land van herkomst</h2>
+            <p>{stekje.landvanherkomst}</p>
+          </div>
+          <div>
+            <h2>Moeilijkheidsgraad</h2>
+            <p>{stekje.categories[0].naam}</p>
+          </div>
+
+          <div>
+            <h2>Water geven</h2>
+            <div
+              className={classes.paragraph}
+              dangerouslySetInnerHTML={{ __html: stekje.watergeven.html }}
+            ></div>
+          </div>
+          {/* <div>
+            <h2>Temperatuur</h2>
+
+            <div
+              className={classes.paragraph}
+              dangerouslySetInnerHTML={{ __html: stekje.temperatuur.html }}
+            ></div>
+          </div>
+          <div>
+            <h2>Zonlicht</h2>
+            <div
+              className={classes.paragraph}
+              dangerouslySetInnerHTML={{ __html: stekje.zonlicht.html }}
+            ></div>
+          </div>
+          <div>
+            <h2>Stekken</h2>
+            <div
+              className={classes.paragraph}
+              dangerouslySetInnerHTML={{ __html: stekje.stekken.html }}
+            ></div>
+          </div> */}
+          <div>
+            <h2>Voeding</h2>
+            <p>{stekje.voeding}</p>
+          </div>
+          <div>
+            <h2>Giftig</h2>
+            <p>{stekje.giftig}</p>
+          </div>
+        </section>
+      </main>
       <Footer />
     </>
   );
@@ -31,10 +110,30 @@ const QUERY = gql`
       id
       naam
       slug
+      categories {
+        naam
+      }
+      landvanherkomst
+      voeding
+      verpotten
+      giftig
+
       fotos {
         url
       }
+      temperatuur {
+        html
+      }
       zonlicht {
+        html
+      }
+      beschrijving {
+        html
+      }
+      stekken {
+        html
+      }
+      watergeven {
         html
       }
     }
