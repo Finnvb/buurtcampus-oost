3f14bd17bd88dc79e2e6abd7366cd8688f3f8e80
Finnvb <finnvb@gmail.com>
2023-03-27 15:27:04 +0200

    Update layout.js

--- plantswap/components/layout.js
+++ plantswap/components/layout.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import NavBar from "./NavBar";
 import Footer from "./Footer";
-function layout({ children, title = "Plantswap" }) {
+function Layout({ children, title = "Plantswap" }) {
   return (
     <>
       <div>
@@ -27,4 +27,4 @@ function layout({ children, title = "Plantswap" }) {
   );
 }
 
-export default layout;
+export default Layout;
